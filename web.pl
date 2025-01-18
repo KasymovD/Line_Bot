e9397a75cfb1935999c7feeca4756d3eb49fd550
KasymovD <ksmv982@gmail.com>
2025-01-18 23:53:15 +0800

    update

--- web.pl
+++ web.pl
@@ -6,7 +6,6 @@ set serializer => 'JSON';
 my $dsn = "DBI:mysql:database=perl_web;host=localhost";
 my $username = "root";
 my $password = "";
-
 my $dbh = DBI->connect($dsn, $username, $password, { RaiseError => 1, AutoCommit => 1 });
 
 get '/' => sub {
@@ -20,7 +19,7 @@ get '/users' => sub {
     while (my $row = $sth->fetchrow_hashref) {
         push @users, $row;
     }
-    return to_json(\@users);
+    return \@users;
 };
 
 get '/users/:id' => sub {
@@ -28,7 +27,7 @@ get '/users/:id' => sub {
     my $sth = $dbh->prepare("SELECT * FROM users WHERE id = ?");
     $sth->execute($id);
     my $user = $sth->fetchrow_hashref;
-    return to_json($user);
+    return $user;
 };
 
 post '/users' => sub {
@@ -37,7 +36,7 @@ post '/users' => sub {
     my $email = $data->{email};
     my $sth = $dbh->prepare("INSERT INTO users (name, email) VALUES (?, ?)");
     $sth->execute($name, $email);
-    return to_json({ status => 'User created' });
+    return { status => 'User created' };
 };
 
 put '/users/:id' => sub {
@@ -47,14 +46,53 @@ put '/users/:id' => sub {
     my $email = $data->{email};
     my $sth = $dbh->prepare("UPDATE users SET name = ?, email = ? WHERE id = ?");
     $sth->execute($name, $email, $id);
-    return to_json({ status => 'User updated' });
+    return { status => 'User updated' };
 };
 
 del '/users/:id' => sub {
     my $id = route_parameters->get('id');
     my $sth = $dbh->prepare("DELETE FROM users WHERE id = ?");
     $sth->execute($id);
-    return to_json({ status => 'User deleted' });
+    return { status => 'User deleted' };
+};
+
+get '/users/search/:name' => sub {
+    my $name = route_parameters->get('name');
+    my $sth = $dbh->prepare("SELECT * FROM users WHERE name LIKE ?");
+    $sth->execute("%$name%");
+    my @users;
+    while (my $row = $sth->fetchrow_hashref) {
+        push @users, $row;
+    }
+    return \@users;
+};
+
+get '/users/count' => sub {
+    my $sth = $dbh->prepare("SELECT COUNT(*) AS count FROM users");
+    $sth->execute();
+    my $result = $sth->fetchrow_hashref;
+    return $result;
+};
+
+patch '/users/:id' => sub {
+    my $id = route_parameters->get('id');
+    my $data = request->data;
+    my @fields;
+    my @values;
+    if (exists $data->{name}) {
+        push @fields, "name = ?";
+        push @values, $data->{name};
+    }
+    if (exists $data->{email}) {
+        push @fields, "email = ?";
+        push @values, $data->{email};
+    }
+    return { status => 'No fields to update' } unless @fields;
+    my $sql = "UPDATE users SET " . join(", ", @fields) . " WHERE id = ?";
+    push @values, $id;
+    my $sth = $dbh->prepare($sql);
+    $sth->execute(@values);
+    return { status => 'User partially updated' };
 };
 
 dance;
